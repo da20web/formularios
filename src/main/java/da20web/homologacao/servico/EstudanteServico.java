bc72bff65b91b43699aa1e1006ba3696899d5592
Your Name <you@example.com>
2023-11-10 10:17:53 -0300

    Criação do package servico e da classe EstudanteRepositorio

---
+++ src/main/java/da20web/homologacao/servico/EstudanteServico.java
@@ -0,0 +1,18 @@
+package da20web.homologacao.servico;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import da20web.homologacao.modelo.Estudante;
+import da20web.homologacao.repositorio.EstudanteRepositorio;
+
+@Service
+public class EstudanteServico {
+
+	@Autowired
+	private EstudanteRepositorio estudanteRepositorio;
+	
+	public Estudante criarEstudante(Estudante estudante) {
+		return estudanteRepositorio.save(estudante);
+	}
+}
